d8264caf26109fa0c3fd149bcc5831c5d3b78951
TheSingingFox <singingfox123@protonmail.com>
2022-02-12 11:42:34 +0100

    Correct hash for Release 0.1.1.3

--- guix.scm
+++ guix.scm
@@ -21,7 +21,7 @@
                (url "https://github.com/silasfox/guile-monads")
                (commit "0.1.1.3")))
         (file-name "guile-monads-0.1.1.3-checkout")
-        (sha256 (base32 "0vqmxkl825nm1ha5gipmqs70gx9r1vrkki485ssfhl20zf8a9zh3"))))
+        (sha256 (base32 "0wp9yypfwdzfvi5ii1l0nwr4agga53z0w68dvryfi3s0p930i322"))))
     (build-system gnu-build-system)
     (arguments `())
     (native-inputs
